ad909a6c3a4e9874c50b6815c4799eef8773f713
jayshiai <jayvardhanpatil7811@gmail.com>
2023-06-02 20:26:26 +0530

    init 4

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,7 +4,7 @@ export const metadata = {
   title: "Promptopia",
   discription: "Discover & Share AI Prompts",
 };
-const RootLayout = () => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,20 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI powered promps</span>
+      </h1>
+      <p className="desc text-center">
+        {" "}
+        Promptopia is a modern proompting technology that allows the world to
+        share best proompts.
+      </p>
+
+      {/* Feed */}
+    </section>
+  );
 };
 
 export default Home;
