8028b3983bb496de3fc0b9edc820c77170d1225a
jayshiai <jayvardhanpatil7811@gmail.com>
2023-06-02 20:03:42 +0530

    init 3

---
+++ src/app/layout.jsx
@@ -0,0 +1,20 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  discription: "Discover & Share AI Prompts",
+};
+const RootLayout = () => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient"></div>
+          <main className="app">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
